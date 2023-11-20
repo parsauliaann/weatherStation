635b6a4bea34655bd275da6742e383466ee11b28
Parsaulian L <lian.parsau@gmail.com>
2023-11-20 14:54:21 +0700

    error dikit

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,7 +4,7 @@
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <link rel="apple-touch-icon" sizes="76x76" href="{{assets('img/apple-icon.png')}}">
-  <link rel="icon" type="image/png" href="{{'img/gundar.png'}}">
+  <link rel="icon" type="image/png" href="{{assets('img/gundar.png')}}">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css">
   <link rel="stylesheet" href="https://cdn.datatables.net/1.13.7/css/dataTables.bootstrap5.min.css">
   <title>
