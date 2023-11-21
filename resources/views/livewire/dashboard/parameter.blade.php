7c43592e8e4bf0094685ff32942c8461127f83bc
Hanafi Abdilah <hanafiabdilah@Hanafis-MacBook-Air.local>
2023-11-21 15:14:40 +0700

    ok

--- resources/views/livewire/dashboard/parameter.blade.php
+++ resources/views/livewire/dashboard/parameter.blade.php
@@ -1,4 +1,4 @@
 
-<div>
+<div wire:poll.1000ms='refresh'>
     <h3 class="font-weight-bolder d-inline" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-custom-class="custom-tooltip" data-bs-title="{{ $sensorData?->time ?? 'No Data' }}" wire:poll.1000ms='refresh'>{{ $sensorData?->value ?? '-' }}{{ $unit }}</h3>
 </div>
