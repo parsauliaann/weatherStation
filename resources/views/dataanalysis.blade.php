ab9110d7d02a29a40bdc210e3099a41c76df145d
Hanafi Abdilah <hanafiabdilah@Hanafis-MacBook-Air.local>
2023-11-29 13:56:08 +0700

    Fix chart

--- resources/views/dataanalysis.blade.php
+++ resources/views/dataanalysis.blade.php
@@ -607,153 +607,101 @@
     var ctx1 = document.getElementById("chart-line").getContext("2d");
     var ctx2 = document.getElementById("chart-line2").getContext("2d");
 
-    var gradientStroke1 = ctx1.createLinearGradient(0, 230, 0, 50);
+    let options = {
+        responsive: true,
+        maintainAspectRatio: false,
+        plugins: {
+            legend: {
+            display: true,
+            }
+        },
+        interaction: {
+            intersect: false,
+            mode: 'index',
+        },
+        scales: {
+            y: {
+            grid: {
+                drawBorder: false,
+                display: true,
+                drawOnChartArea: true,
+                drawTicks: false,
+                borderDash: [5, 5]
+            },
+            ticks: {
+                display: true,
+                padding: 10,
+                color: '#ccc',
+                font: {
+                size: 11,
+                family: "Open Sans",
+                style: 'normal',
+                lineHeight: 2
+                },
+            }
+            },
+            x: {
+            grid: {
+                drawBorder: false,
+                display: false,
+                drawOnChartArea: false,
+                drawTicks: false,
+                borderDash: [5, 5]
+            },
+            ticks: {
+                display: true,
+                color: '#ccc',
+                padding: 20,
+                font: {
+                size: 11,
+                family: "Open Sans",
+                style: 'normal',
+                lineHeight: 2
+                },
+            }
+            },
+        },
+    }
 
-    gradientStroke1.addColorStop(1, 'rgba(94, 114, 228, 0.2)');
-    gradientStroke1.addColorStop(0.2, 'rgba(94, 114, 228, 0.0)');
-    gradientStroke1.addColorStop(0, 'rgba(94, 114, 228, 0)');
-
-    let chart2 = new Chart(ctx2, {
+    let chart = new Chart(ctx1, {
         type: "line",
         data: {
-            dataset:[
+            labels: ['1', '2'],
+            datasets: [
                 {
-                    label: "Wind Direction",
+                    label: "Wind Speed",
                     borderWidth: 0,
                     pointRadius: 2,
-                    borderColor: "#ff6721",
-                    backgroundColor: gradientStroke1,
+                    borderColor: "#5e72e4",
                     borderWidth: 2,
                     fill: true,
-                    data: [],
+                    data: [1, 2],
                     maxBarThickness: 6
                 }
             ],
         },
-        options: {}
+        options: options,
     });
 
-    let chart = new Chart(ctx1, {
-    type: "line",
-    data: {
-        datasets: [
-        {
-            label: "Wind Speed",
-            borderWidth: 0,
-            pointRadius: 2,
-            borderColor: "#5e72e4",
-            backgroundColor: gradientStroke1,
-            borderWidth: 2,
-            fill: true,
-            data: [],
-            maxBarThickness: 6
-        }
-    ],
-    },
-    options: {
-        responsive: true,
-        maintainAspectRatio: false,
-        plugins: {
-          legend: {
-            display: true,
-          }
+    let chart2 = new Chart(ctx2, {
+        type: "line",
+        data: {
+            labels: ['1', '2'],
+            datasets: [
+                {
+                    label: "Wind Direction",
+                    borderWidth: 0,
+                    pointRadius: 2,
+                    borderColor: "#5e72e4",
+                    borderWidth: 2,
+                    fill: true,
+                    data: [1, 2],
+                    maxBarThickness: 6
+                }
+            ],
         },
-        interaction: {
-          intersect: false,
-          mode: 'index',
-        },
-        scales: {
-          y: {
-            grid: {
-              drawBorder: false,
-              display: true,
-              drawOnChartArea: true,
-              drawTicks: false,
-              borderDash: [5, 5]
-            },
-            ticks: {
-              display: true,
-              padding: 10,
-              color: '#ccc',
-              font: {
-                size: 11,
-                family: "Open Sans",
-                style: 'normal',
-                lineHeight: 2
-              },
-            }
-          },
-          x: {
-            grid: {
-              drawBorder: false,
-              display: false,
-              drawOnChartArea: false,
-              drawTicks: false,
-              borderDash: [5, 5]
-            },
-            ticks: {
-              display: true,
-              color: '#ccc',
-              padding: 20,
-              font: {
-                size: 11,
-                family: "Open Sans",
-                style: 'normal',
-                lineHeight: 2
-              },
-            }
-          },
-        },
-      },
+        options: options,
     });
-
-    (async () => {
-      const response = await fetch("/dashboard", {
-        headers: {
-          "Content-Type": "application/json",
-        }
-      });
-
-      const responseJson = await response.json();
-
-      chart.data.labels = responseJson.labels;
-      chart.data.datasets.forEach((dataset, index) => {
-          dataset.data = responseJson.data[dataset.label.replace(' ', '_').toLowerCase()]
-      });
-
-      chart.update();
-    })()
-
-    setInterval(() => {
-      const last = chart.data.labels[chart.data.labels.length - 1];
-
-      (async () => {
-        const response = await fetch("/dashboard?last=" + last, {
-          headers: {
-            "Content-Type": "application/json",
-          }
-        });
-
-        const responseJson = await response.json();
-
-        if(responseJson.label){
-          if(chart.data.labels.length >= 20){
-            chart.data.labels.splice(0, 1);
-            chart.data.datasets.forEach((dataset) => {
-              dataset.data.splice(0, 1);
-            });
-          }
-
-          chart.data.labels.push(responseJson.label)
-          chart.data.datasets.forEach((dataset, index) => {
-              dataset.data.push(responseJson.data[index]);
-          });
-
-          chart.update();
-        }
-      })()
-
-    }, 10000);
   </script>
   <script>
     const triggerTabList = [].slice.call(document.querySelectorAll('#myTab a'));
